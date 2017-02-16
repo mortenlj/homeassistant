0a1a4d274aab41588fe8143d8b075e4112f7d98c
Morten Lied Johansen <mortenjo@ifi.uio.no>
2017-02-16 22:26:56 +0100

    Test-scripts

--- enter.sh
+++ enter.sh
@@ -9,12 +9,12 @@ readonly CONTAINER_NAME="babushka-test"
 
 if [[ -n $(docker ps --filter "name=${CONTAINER_NAME}" -q) ]]; then
     echo Container is running
-    docker exec -ti "${CONTAINER_NAME}" /bin/bash -c "${TO}/init.sh"
+    docker exec -ti "${CONTAINER_NAME}" /bin/bash
 elif [[ -n $(docker ps -a --filter "name=${CONTAINER_NAME}" -q) ]]; then
     echo Container exists, but is not running
     docker start "${CONTAINER_NAME}"
-    docker exec -ti "${CONTAINER_NAME}" /bin/bash -c "${TO}/init.sh"
+    docker exec -ti "${CONTAINER_NAME}" /bin/bash
 else
     echo Container does not exist
-    docker run --name  "${CONTAINER_NAME}" -ti -v "${ROOTDIR}":"${TO}" ubuntu:xenial /bin/bash -c "${TO}/init.sh"
+    docker run --name  "${CONTAINER_NAME}" -ti -v "${ROOTDIR}":"${TO}" ubuntu:xenial /bin/bash 
 fi
