9e70290aa44643922481e8fc530c8adb5eb7fd79
Morten Lied Johansen <mortenjo@ifi.uio.no>
2017-08-19 22:21:33 +0200

    Update configuration, add prometheus metrics

--- services/install.sh
+++ services/install.sh
@@ -21,7 +21,7 @@ ha_req "homeassistant.components.sensor.yahoo_finance"
 ha_req "homeassistant.components.sensor.yr"
 ha_req "homeassistant.components.discovery"
 ha_req "homeassistant.components.updater"
-ha_req "homeassistant.components.device_tracker.linksys_ap"
+ha_req "homeassistant.components.prometheus"
 
 apk del .build-deps
 
