200493d20eccc316bc93d8865b6f6bf3f381f52b
Morten Lied Johansen <mortenjo@ifi.uio.no>
2019-10-27 23:10:52 +0100

    Update and fix config and deploy to k3s

--- fabfile.py
+++ fabfile.py
@@ -3,38 +3,23 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
 import os
-import shutil
-import tarfile
 import tempfile
-import time
 from fnmatch import fnmatch
 
 import requests
-from fabric.api import task, local, env, runs_once, execute, hosts
-from fabric.context_managers import quiet, cd, lcd, shell_env
-from fabric.contrib.files import append
+from fabric.api import task, local, execute, hosts
+from fabric.context_managers import quiet, cd
+from fabric.contrib.files import upload_template
 from fabric.operations import put, run, sudo, get
-from fabric.utils import error
 
 REQUIREMENTS = [
     "Fabric==1.14.0",
     "requests==2.19.1"
 ]
 
-# Download kernel from https://github.com/dhruvvyas90/qemu-rpi-kernel/blob/master/kernel-qemu-4.4.34-jessie
-# Download img from https://blog.hypriot.com/downloads/
-# Adjust image according to https://blogs.msdn.microsoft.com/iliast/2016/11/10/how-to-emulate-raspberry-pi/
-BABUSHKA_REPO = "https://github.com/benhoskings/babushka.git"
-BABUSHKA_TAG = "v0.19.2"
 LOCAL_DIR = os.path.abspath(os.path.dirname(__file__))
 LOCAL_PATH, LOCAL_NAME = os.path.split(LOCAL_DIR)
 BUILD_VERSION_FILE = ".build_version"
-WHEEL_CACHE = "services/.wheel_cache"
-
-env.roledefs = {
-    "test": ["pirate@localhost:5022"],
-    "prod": ["pirate@home-assistant"]
-}
 
 
 def _find_file(pattern):
@@ -64,86 +49,13 @@ def _get_homeassistant_version():
     return data["info"]["version"]
 
 
-@task
-def start_vm():
-    if "test" in env.effective_roles:
-        kernel = _find_file("kernel-qemu*")
-        image = _find_file("hypriotos-rpi-*.img")
-        local('qemu-system-arm -kernel {} -cpu arm1176 -m 256 -M versatilepb -daemonize --no-reboot --no-shutdown'
-              ' -append "root=/dev/sda2 panic=1 rootfstype=ext4 rw" -redir tcp:5022::22 -redir tcp:5080::80'
-              ' -redir tcp:5081::8080 -drive file={},format=raw'.format(kernel, image))
-        time.sleep(60)
-        execute(install_ssh_key)
-        sudo("apt-get update")
+def _get_last_built_version():
+    with open(BUILD_VERSION_FILE) as fobj:
+        return fobj.read().strip()
 
 
 @task
-@runs_once
-def install_ssh_key():
-    key = local("ssh-add -L | grep ibidem", capture=True).strip()
-    append("~/.ssh/authorized_keys", key)
-
-
-@task
-@runs_once
-def build():
-    with lcd("services"):
-        version = _get_homeassistant_version()
-        if not os.path.isdir(WHEEL_CACHE):
-            os.makedirs(WHEEL_CACHE)
-        local("docker run --rm --privileged multiarch/qemu-user-static:register --reset")
-        local("docker build --build-arg HOME_ASSISTANT_VERSION={0}"
-              " -t mortenlj/home-assistant-rpi:{0} .".format(version))
-        local("docker push mortenlj/home-assistant-rpi")
-        _extract_layers(version)
-        with open(BUILD_VERSION_FILE, "w") as fobj:
-            fobj.write(version)
-
-
-def _extract_layers(version):
-    save = tempfile.mkdtemp(prefix="dockersave")
-    local("docker image save mortenlj/home-assistant-rpi:{} -o {}/image.tar".format(version, save))
-    with tarfile.open(os.path.join(save, "image.tar")) as image_tar:
-        image_tar.extractall(save, (ti for ti in image_tar if ti.name.endswith("layer.tar")))
-    for dirpath, dirnames, filenames in os.walk(save):
-        for filename in filenames:
-            if filename == "layer.tar":
-                filepath = os.path.join(dirpath, filename)
-                with tarfile.open(filepath) as tar:
-                    members = [ti for ti in tar if os.path.splitext(ti.name)[-1] == ".whl"]
-                    print("Found {} wheels in {}".format(len(members), filepath))
-                    for member in members:
-                        src = tar.extractfile(member)
-                        dst_path = os.path.join(WHEEL_CACHE, os.path.basename(member.name))
-                        print("Extracting {} to {}".format(member.name, dst_path))
-                        with open(dst_path, "wb") as dst:
-                            shutil.copyfileobj(src, dst)
-
-
-@task
-def deploy_code():
-    execute(install_ssh_key)
-    tar_file = "%s.tar.gz" % LOCAL_NAME
-    target_tar = os.path.join("/tmp", tar_file)
-    tmp_folder = tempfile.mkdtemp()
-
-    try:
-        tar_path = os.path.join(tmp_folder, tar_file)
-        local("tar -czf %s --exclude=dev-tools --exclude-vcs-ignores --exclude-vcs -C %s %s" % (
-        tar_path, LOCAL_PATH, LOCAL_NAME))
-        put(tar_path, target_tar)
-        with cd("/tmp"):
-            try:
-                run("rm -rf %s" % LOCAL_NAME)
-                run("tar -xzf %s" % tar_file)
-            finally:
-                run("rm -f %s" % tar_file)
-    finally:
-        local("rm -rf %s" % tmp_folder)
-
-
-@task
-@hosts("emrys")
+@hosts("root@emrys")
 def deploy_configuration():
     tar_file = "homeassistant_configuration.tar.gz"
     target_dir = "/volume1/k3s_persistent_volumes/homeassistant"
@@ -153,69 +65,39 @@ def deploy_configuration():
 
     try:
         tar_path = os.path.join(tmp_folder, tar_file)
-        local("tar -czf %s --exclude-vcs -C %s ." % (tar_path,  source_path))
+        local("tar -czf %s --exclude-vcs -C %s ." % (tar_path, source_path))
         run("mkdir -p %s" % target_dir)
         put(tar_path, target_tar)
         with cd(target_dir):
             try:
-                run("tar -xzf %s" % tar_file)
+                run("tar --overwrite --no-same-owner --no-same-permissions -xzf %s" % tar_file)
             finally:
                 run("rm -f %s" % tar_file)
     finally:
         local("rm -rf %s" % tmp_folder)
+        target_dir = "/volume1/k3s_persistent_volumes/homeassistant"
+        run("chown -R admin:users %s" % target_dir)
+        run("chmod -R 775 %s" % target_dir)
 
 
 @task
-@hosts("root@emrys")
-def _fix_configuration_ownership():
-    target_dir = "/volume1/k3s_persistent_volumes/homeassistant"
-    run("chown -R root:root %s" % target_dir)
-    run("chmod -R 775 %s" % target_dir)
-
-@task
-def rm_docker():
-    execute(install_ssh_key)
-    sudo("systemctl stop home-assistant.service")
-    sudo("docker images mortenlj/home-assistant-rpi -q | xargs --no-run-if-empty docker rmi")
-    sudo("rm /var/lib/docker-compose/home-assistant.yml")
-
-
-def _get_last_built_version():
-    with open(BUILD_VERSION_FILE) as fobj:
-        return fobj.read().strip()
+@hosts("rancher@rpi3b02")
+def deploy():
+    for f in os.listdir("kubernetes"):
+        path = os.path.join("kubernetes", f)
+        destination = "/var/lib/rancher/k3s/server/manifests/homeassistant-{}".format(f)
+        upload_template(path, destination, {"version": _get_last_built_version()}, use_sudo=True, backup=False)
 
 
 @task
-def babushka(reinstall=False, version=None):
-    if version is None:
-        version = _get_last_built_version()
-    execute(install_ssh_key)
-    remote_dir = os.path.join("/tmp", LOCAL_NAME)
-    with cd(remote_dir):
-        if _is_not_root() and (not _executable("sudo")):
-            error("Not root, and sudo is not available. This needs some human interaction.")
-        if not _executable("git"):
-            sudo("apt-get install -qqy git")
-        if not _executable("ruby"):
-            sudo("apt-get install -qqy ruby")
-        if not _executable("babushka") or reinstall:
-            with quiet():
-                sudo("rm -rf /opt/babushka")
-            sudo("mkdir -p /opt")
-            sudo("git clone {} /opt/babushka --depth 1 --branch {}".format(BABUSHKA_REPO, BABUSHKA_TAG))
-            with cd("/usr/local/bin"):
-                sudo("ln -sf /opt/babushka/bin/babushka.rb babushka")
-            if not _executable("babushka"):
-                error("Failed to install babushka!")
-        with shell_env(HOME_ASSISTANT_VERSION=version):
-            sudo("babushka main")
+def update_version():
+    version = _get_homeassistant_version()
+    with open(BUILD_VERSION_FILE, "w") as fobj:
+        fobj.write(version)
 
 
 @task
+@hosts("emrys")
 def update_known_devices():
-    get("/etc/home-assistant/known_devices.yaml", "babushka-deps/renderables/home-assistant/known_devices.yaml")
-
-
-@task
-def restart_service(name="home-assistant"):
-    sudo("systemctl restart {}.service".format(name))
+    get("/k3s_persistent_volumes/homeassistant/known_devices.yaml",
+        "babushka-deps/renderables/home-assistant/known_devices.yaml")
